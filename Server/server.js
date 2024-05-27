5b72a79dc0f6a3b48c1d820acd0a9ec2d90d285b
AflahAhamed00 <aflahahamed777@gmail.com>
2024-05-27 12:49:30 +0530

    completed

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ import dbConnection from "./dbConnection.js"
 dbConnection()
 
 app.use(express.json())
-app.use(cors({origin:"https://machine-test-ecommerce-hrf6.vercel.app"}))
+app.use(cors())
 app.use("/",route)
 
 app.listen(5000,()=>{console.log("server listen in port 5000")})
